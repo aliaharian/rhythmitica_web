72fc43534d30d30f2253c2daf2e072b6bb1854d1
ali izadi <ali.izadi@chista.ir>
2022-01-12 23:02:33 +0330

    booking done

--- pages/partners.js
+++ pages/partners.js
@@ -1,23 +1,23 @@
-import Layout from '../src/components/layout/Layout'
-import { wrapper } from "../redux/store";
-import Partners from '../src/components/partners/Partners';
+// import Layout from '../src/components/layout/Layout'
+// import { wrapper } from "../redux/store";
+// import Partners from '../src/components/partners/Partners';
 
-function PartnersPage() {
-  return (
-    <Layout page={'partners'}>
-      <Partners />
-    </Layout>
-  )
-}
-export const getServerSideProps = wrapper.getServerSideProps(
-  (store) =>
-    async ({ req, res }) => {
+// function PartnersPage() {
+//   return (
+//     <Layout page={'partners'}>
+//       <Partners />
+//     </Layout>
+//   )
+// }
+// export const getServerSideProps = wrapper.getServerSideProps(
+//   (store) =>
+//     async ({ req, res }) => {
      
-      return {
-        props: {
-          page: 'partners',
-        }, // will be passed to the page component as props
-      };
-    });
+//       return {
+//         props: {
+//           page: 'partners',
+//         }, // will be passed to the page component as props
+//       };
+//     });
 
-export default PartnersPage;
+// export default PartnersPage;
