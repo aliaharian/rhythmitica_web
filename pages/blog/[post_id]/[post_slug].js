8caf41f9ae50b6678a4cab09c99b52ee5e17360b
ali izadi <ali.izadi@chista.ir>
2022-01-12 23:07:25 +0330

    booking done

--- pages/blog/[post_id]/[post_slug].js
+++ pages/blog/[post_id]/[post_slug].js
@@ -1,38 +1,38 @@
-// import Layout from '../../../src/components/layout/Layout'
-// import { wrapper } from "../../../redux/store";
-// import axios from 'axios'
-// import SinglePost from '../../../src/components/singlePost/SinglePost';
+import Layout from '../../../src/components/layout/Layout'
+import { wrapper } from "../../../redux/store";
+import axios from 'axios'
+import SinglePost from '../../../src/components/singlePost/SinglePost';
 
-// function singlePostPage({ postInfo }) {
-//     return (
-//         <Layout page={'singleEvent'}>
-//             <SinglePost postInfo={postInfo} />
-//         </Layout>
-//     )
-// }
-// export const getServerSideProps = wrapper.getServerSideProps(
-//     (store) =>
-//         async ({ req, res, query }) => {
-
-
-//             try {
+function singlePostPage({ postInfo }) {
+    return (
+        <Layout page={'singleEvent'}>
+            {/* <SinglePost postInfo={postInfo} /> */}
+        </Layout>
+    )
+}
+export const getServerSideProps = wrapper.getServerSideProps(
+    (store) =>
+        async ({ req, res, query }) => {
 
 
-//                 const postInfo = await axios.get(`${process.env.REACT_APP_BASE_URL}/v1.0/blog/${query.post_slug}`, null, {
-//                     headers: {
-//                         site: process.env.REACT_APP_SITE_TOKEN
-//                     },
-//                 });
+            try {
 
-//                 return {
-//                     props: {
-//                         postInfo: postInfo.data,
-//                     }, // will be passed to the page component as props
-//                 };
-//             }
-//             catch (e) {
-//                 console.log(e)
-//             }
-//         });
 
-// export default singlePostPage;
+                const postInfo = await axios.get(`${process.env.REACT_APP_BASE_URL}/v1.0/blog/${query.post_slug}`, null, {
+                    headers: {
+                        site: process.env.REACT_APP_SITE_TOKEN
+                    },
+                });
+
+                return {
+                    props: {
+                        postInfo: postInfo.data,
+                    }, // will be passed to the page component as props
+                };
+            }
+            catch (e) {
+                console.log(e)
+            }
+        });
+
+export default singlePostPage;
--- pages/contactUs.js
+++ pages/contactUs.js
@@ -1,23 +1,23 @@
-// import Layout from '../src/components/layout/Layout'
-// import { wrapper } from "../redux/store";
-// import Contact from '../src/components/contact/Contact';
+import Layout from '../src/components/layout/Layout'
+import { wrapper } from "../redux/store";
+import Contact from '../src/components/contact/Contact';
 
-// function contactUsPage() {
-//   return (
-//     <Layout page={'contact'}>
-//       <Contact />
-//     </Layout>
-//   )
-// }
-// export const getServerSideProps = wrapper.getServerSideProps(
-//   (store) =>
-//     async ({ req, res }) => {
+function contactUsPage() {
+  return (
+    <Layout page={'contact'}>
+      {/* <Contact /> */}
+    </Layout>
+  )
+}
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) =>
+    async ({ req, res }) => {
      
-//       return {
-//         props: {
-//           page: 'contact',
-//         }, // will be passed to the page component as props
-//       };
-//     });
+      return {
+        props: {
+          page: 'contact',
+        }, // will be passed to the page component as props
+      };
+    });
 
-// export default contactUsPage;
+export default contactUsPage;
