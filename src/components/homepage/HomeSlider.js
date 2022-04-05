ac49e29d828c8bf46b26fc41b7016e0d9ca940a1
aliaharian <aliaharian@yahoo.com>
2022-04-05 11:50:34 +0430

    change slider images

--- src/components/homepage/HomeSlider.js
+++ src/components/homepage/HomeSlider.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 import classes from '../../assets/styleSheets/homepage.module.scss';
-import slide1 from '../../assets/images/slider/slide1.jpg';
+import slide0 from '../../assets/images/slider/slide1.jpg';
+
+import slide1 from '../../assets/images/slider/1.jpg';
+import slide2 from '../../assets/images/slider/2.jpg';
+import slide3 from '../../assets/images/slider/3.jpg';
+import slide4 from '../../assets/images/slider/4.jpg';
+
 const HomeSlider = () => {
     const settings = {
         dots: true,
@@ -20,10 +26,13 @@ const HomeSlider = () => {
                     <img src={slide1}/>
                 </div>
                 <div className={classes.slideItem}>
-                    <img src={slide1}/>
+                    <img src={slide2}/>
                 </div>
                 <div className={classes.slideItem}>
-                    <img src={slide1}/>
+                    <img src={slide3}/>
+                </div>
+                <div className={classes.slideItem}>
+                    <img src={slide4}/>
                 </div>
                 
             </Slider>
