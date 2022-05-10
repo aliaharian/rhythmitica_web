cd420947daca2655290a2ae9941ef28b668472e8
aliaharian <aliaharian@yahoo.com>
2022-05-10 12:57:33 +0430

    blog ssg

--- pages/blog/[post_id]/[post_slug].js
+++ pages/blog/[post_id]/[post_slug].js
@@ -35,7 +35,7 @@ export async function getStaticProps({ params }) {
     try {
         const postInfo = await axios.get(`${process.env.REACT_APP_BASE_URL}/v1.0/blog/${params.post_slug}`, null, {
             headers: {
-                site: process.env.REACT_APP_SITE_TOKEN
+                site: '6e6a90a95755444cbdee6d0df6929539' //process.env.REACT_APP_SITE_TOKEN
             },
         });
 
