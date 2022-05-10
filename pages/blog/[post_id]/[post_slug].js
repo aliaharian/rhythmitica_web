b31bdaf5193f6aee0367548033155512e552787a
aliaharian <aliaharian@yahoo.com>
2022-05-10 15:26:02 +0430

    ssg

--- pages/blog/[post_id]/[post_slug].js
+++ pages/blog/[post_id]/[post_slug].js
@@ -15,7 +15,7 @@ function singlePostPage({ postInfo }) {
 export async function getStaticPaths() {
     const posts = await axios.get(`${process.env.REACT_APP_BASE_URL}/v1.0/blog/briefList`, {
         headers: {
-            site: process.env.REACT_APP_SITE_TOKEN
+            site: '6e6a90a95755444cbdee6d0df6929539' //process.env.REACT_APP_SITE_TOKEN
         },
     });
 
