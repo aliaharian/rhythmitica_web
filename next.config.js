9f9ff4babae0ecbc45c305c155e48e62153c8fee
aliaharian <aliaharian@yahoo.com>
2022-05-23 10:01:17 +0430

    sharp 4 images

--- next.config.js
+++ next.config.js
@@ -13,8 +13,9 @@ module.exports = withImages(withFonts({
     },
     images: {
         deviceSizes: [1080, 1200, 1440, 1920, 2048, 3840],
-        disableStaticImages: true,
+        // disableStaticImages: true,
         domains: ['itica.ca'],
+        path: '_next/image'
 
     },
 }))
